1f465ab05734f09d5ec3ec8aa43965dc3d7c84c5
sakulstra <lukasstrassel@googlemail.com>
2024-08-09 21:25:47 +0200

    fix: revert changes to make andrey happy

---
+++ etherscan/stkAAVE/StakedAaveV3/src/lib/GovernancePowerWithSnapshot.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+/**
+ * @title MOCK CONTRACT TO KEEP VALID STORAGE LAYOUT
+ * @dev WAS including snapshots of balances on transfer-related actions
+ * @author BGD Labs
+ **/
+abstract contract GovernancePowerWithSnapshot {
+  uint256[3] private __________DEPRECATED_GOV_V2_PART;
+}
