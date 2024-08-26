f1484d5e5e485f5cf60d21747f1e11f9414b209e
pavelvm5 <menshikov.pavelvm@gmail.com>
2024-08-26 15:13:38 +0500

    deleted excess function

--- tests/utils/StakeTestBase.sol
+++ tests/utils/StakeTestBase.sol
@@ -110,20 +110,6 @@ contract StakeTestBase is Test {
     return 3;
   }
 
-  // function checkPowerLoss(uint256 expected, uint256 get) internal pure returns (uint256 power) {
-  //   uint256 diff = getDiff(expected, get);
-
-  //   while (true) {
-  //     diff = diff / 10;
-
-  //     if (diff == 0) {
-  //       return power;
-  //     }
-
-  //     power++;
-  //   }
-  // }
-
   function getDiff(uint256 a, uint256 b) internal pure returns (uint256) {
     return a > b ? a - b : b - a;
   }
